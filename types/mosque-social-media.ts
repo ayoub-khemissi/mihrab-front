f54ccf01950edb02c4d5c5018201a764fb553425
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-05-12 15:38:40 +0200

    fix: Mosque social media type remove mosque entity

--- types/mosque-social-media.ts
+++ types/mosque-social-media.ts
@@ -1,7 +1,4 @@
-import { Mosque } from "./mosque";
-
 export type MosqueSocialMedia = {
-  mosque: Mosque;
   platform: "website" | "instagram" | "facebook" | "youtube" | "twitter";
   url: string;
   created_at: number;
