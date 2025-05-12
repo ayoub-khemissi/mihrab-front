9f860ba91d8d212e73a5137324a88165b4960f00
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-05-12 15:25:49 +0200

    feat: Main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { fontBeVietnamPro, fontDMSerifText } from "@/config/fonts";
+import { Header } from "@/components/header";
+import Container from "@/components/container";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
   icons: {
@@ -37,27 +38,16 @@ export default function RootLayout({
       <head />
       <body
         className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          "min-h-screen bg-secondary font-beVietnamPro antialiased",
+          fontBeVietnamPro.variable,
+          fontDMSerifText.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+          <div className="relative flex flex-col">
+            <Header />
+            <Container>{children}</Container>
+            <Footer />
           </div>
         </Providers>
       </body>
