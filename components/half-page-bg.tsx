a8a29b875122fbb4548e55509bacc23f07546c22
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-05-14 12:25:43 +0200

    feat: Half page bg component

---
+++ components/half-page-bg.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+
+const randomImageNumber = Math.floor(Math.random() * 13) + 1;
+
+const HalfPageBg = () => {
+  return (
+    <div className="relative flex flex-col items-center rounded-xl gap-y-10 justify-center p-10 h-full bg-tertiary overflow-hidden">
+      <div className="absolute left-0 top-0 h-full w-full bg-[url('/assets/svg/bg-flowers-light.svg')] opacity-80 bg-cover bg-center z-0 pointer-events-none" />
+      <div className="relative w-full h-full rounded-xl overflow-hidden z-10">
+        <Image
+          fill
+          alt="Mosque"
+          className="object-cover rounded-xl transition-transform duration-300"
+          src={`/assets/bg/mosque-${randomImageNumber}.jpg`}
+          style={{ objectPosition: "center" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HalfPageBg;
