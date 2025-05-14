ec5c223dbb17fc6e087e3cf975b66c0d341f9411
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-05-14 12:57:39 +0200

    feat: Showcase page loading component

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Loading from "@/components/loading";
 import HalfPageBg from "@/components/half-page-bg";
 
 export default function Showcase() {
   return (
-    <>
+    <div className="mt-24">
       <HalfPageBg />
-    </>
+      <Loading />
+    </div>
   );
 }
