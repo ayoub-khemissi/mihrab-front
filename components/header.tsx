77d28663ee5851a83de8e15b027a4023cc901504
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-06-15 21:58:51 +0200

    feat: Refactor Header component to include user role-based navigation and improve mobile menu functionality

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,33 @@
 "use client";
 
 import {
-  Navbar as HeroUINavbar,
+  Navbar,
   NavbarContent,
   NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
 } from "@heroui/navbar";
 import clsx from "clsx";
 import { Button } from "@heroui/button";
 import { useEffect, useState } from "react";
 import { Link } from "@heroui/link";
 import { useRouter, useSearchParams } from "next/navigation";
+import { FaUser } from "react-icons/fa6";
 
-import { CrossIcon, Logo, MenuIcon } from "@/components/icons";
+import { Logo } from "@/components/icons";
+import { User } from "@/types/EntityTypes/User";
 
 export const Header = () => {
   const searchParams = useSearchParams();
   const [profileSelected, setProfileSelected] = useState(
     (searchParams.get("profile") as "imam" | "mosque") || "imam",
   );
+  const [user, setUser] = useState<User | null>(null);
 
   const router = useRouter();
 
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleProfileChange = (profile: "imam" | "mosque") => {
     setProfileSelected(profile);
@@ -34,94 +40,258 @@ export const Header = () => {
     if (profile) {
       setProfileSelected(profile as "imam" | "mosque");
     }
+
+    const localUser = localStorage.getItem("user");
+
+    if (localUser) {
+      setUser(JSON.parse(localUser));
+    }
   }, [searchParams]);
+
+  const isImam = user?.role === "imam";
+  const isMosqueManager = user?.role === "mosque_manager";
+  const isAdmin = user?.role === "admin";
 
   return (
     <div className="container mx-auto max-w-7xl">
-      <HeroUINavbar
+      <Navbar
         className="py-4 bg-secondary fixed top-0 z-50 w-full xl:h-24 h-16"
+        isMenuOpen={isMenuOpen}
         maxWidth="xl"
+        onMenuOpenChange={setIsMenuOpen}
       >
-        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-          <NavbarBrand as="li" className="gap-3 max-w-fit">
-            <Link className="flex justify-start items-center gap-1" href="/">
-              <Logo />
-            </Link>
-          </NavbarBrand>
-        </NavbarContent>
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <Link className="flex justify-start items-center gap-1" href="/">
+            <Logo />
+          </Link>
+        </NavbarBrand>
         <NavbarContent justify="end">
-          <div className="hidden md:flex gap-6 justify-start text-nowrap">
-            <Button
-              className={clsx(
-                "border-primary border-2 rounded-sm font-normal",
-                profileSelected === "imam"
-                  ? "bg-primary text-secondary font-medium"
-                  : "bg-transparent text-primary",
+          {user ? (
+            <>
+              {!isImam && (
+                <>
+                  <Link
+                    className="text-primary text-sm font-medium gap-2"
+                    href="/job-offers"
+                  >
+                    <FaUser className="text-primary text-sm font-medium" />
+                    <span className="text-primary text-sm font-medium">
+                      Mosquées
+                    </span>
+                  </Link>
+                  <Link
+                    className="text-primary text-sm font-medium gap-2"
+                    href="/apply"
+                  >
+                    <FaUser className="text-primary text-sm font-medium" />
+                    <span className="text-primary text-sm font-medium">
+                      Postuler
+                    </span>
+                  </Link>
+                  <div className="w-0.5 h-auto bg-black/5" />
+                </>
               )}
-              onPress={() => {
-                handleProfileChange("imam");
-              }}
-            >
-              Je suis Imam
-            </Button>
-            <Button
-              className={clsx(
-                "border-primary border-2 rounded-sm font-normal",
-                profileSelected === "mosque"
-                  ? "bg-primary text-secondary font-medium"
-                  : "bg-transparent text-primary",
+              {isMosqueManager && (
+                <>
+                  <Link
+                    className="text-primary text-sm font-medium gap-2"
+                    href="/home"
+                  >
+                    <FaUser className="text-primary text-sm font-medium" />
+                    <span className="text-primary text-sm font-medium">
+                      Accueil
+                    </span>
+                  </Link>
+                  <div className="w-0.5 h-auto bg-black/5" />
+                  <Link
+                    className="text-primary text-sm font-medium gap-2"
+                    href="/imam"
+                  >
+                    <FaUser className="text-primary text-sm font-medium" />
+                    <span className="text-primary text-sm font-medium">
+                      Imams
+                    </span>
+                  </Link>
+                  <div className="w-0.5 h-auto bg-black/5" />
+                  <Link
+                    className="text-primary text-sm font-medium gap-2"
+                    href="/publish"
+                  >
+                    <FaUser className="text-primary text-sm font-medium" />
+                    <span className="text-primary text-sm font-medium">
+                      Publier
+                    </span>
+                  </Link>
+                  <div className="w-0.5 h-auto bg-black/5" />
+                </>
               )}
-              onPress={() => {
-                handleProfileChange("mosque");
-              }}
-            >
-              Je représente une Mosquée
-            </Button>
-            <div className="w-0.5 h-auto bg-black/5" />
-            <Link className="text-primary text-sm font-medium" href="/login">
-              Se connecter
-            </Link>
-          </div>
-          <div className="flex md:hidden items-center">
-            <button
-              aria-label={mobileMenuOpen ? "Fermer le menu" : "Ouvrir le menu"}
-              className="p-2 rounded focus:outline-none focus:ring-2 focus:ring-primary z-50"
-              onClick={() => setMobileMenuOpen((open) => !open)}
-            >
-              {mobileMenuOpen ? <CrossIcon /> : <MenuIcon />}
-            </button>
-          </div>
-        </NavbarContent>
-      </HeroUINavbar>
-      {mobileMenuOpen && (
-        <div className="fixed top-0 z-40 flex md:hidden w-full h-full">
-          <nav className="bg-primary/95 text-secondary w-full h-full flex flex-col items-center justify-center">
-            <div className="flex flex-col justify-center items-center text-center gap-6">
+              {isAdmin && (
+                <>
+                  <Link
+                    className="text-primary text-sm font-medium gap-2"
+                    href="/home"
+                  >
+                    <FaUser className="text-primary text-sm font-medium" />
+                    <span className="text-primary text-sm font-medium">
+                      Accueil
+                    </span>
+                  </Link>
+                  <div className="w-0.5 h-auto bg-black/5" />
+                </>
+              )}
               <Link
-                className="text-secondary text-2xl font-medium"
+                className="text-primary text-sm font-medium gap-2"
+                href="/profile"
+              >
+                <FaUser className="text-primary text-sm font-medium" />
+                <span className="text-primary text-sm font-medium">
+                  {user.first_name && user.last_name
+                    ? `${user.first_name} ${user.last_name}`
+                    : "Mon profil"}
+                </span>
+              </Link>
+            </>
+          ) : (
+            <div className="hidden md:flex gap-6 justify-start text-nowrap">
+              <Button
+                className={clsx(
+                  "border-primary border-2 rounded-sm font-normal",
+                  profileSelected === "imam"
+                    ? "bg-primary text-secondary font-medium"
+                    : "bg-transparent text-primary",
+                )}
+                onPress={() => {
+                  handleProfileChange("imam");
+                }}
+              >
+                Je suis Imam
+              </Button>
+              <Button
+                className={clsx(
+                  "border-primary border-2 rounded-sm font-normal",
+                  profileSelected === "mosque"
+                    ? "bg-primary text-secondary font-medium"
+                    : "bg-transparent text-primary",
+                )}
+                onPress={() => {
+                  handleProfileChange("mosque");
+                }}
+              >
+                Je représente une Mosquée
+              </Button>
+              <div className="w-0.5 h-auto bg-black/5" />
+              <Link
+                className="text-primary text-sm font-medium gap-2"
                 href="/login"
-                onClick={() => setMobileMenuOpen(false)}
               >
                 Se connecter
               </Link>
-              <Link
-                className="text-secondary text-2xl font-medium"
-                href="/contact"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                Contact
-              </Link>
-              <Link
-                className="text-secondary text-2xl font-medium"
-                href="/faq"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                FAQ
-              </Link>
             </div>
-          </nav>
-        </div>
-      )}
+          )}
+          <NavbarMenuToggle className="md:hidden" />
+        </NavbarContent>
+
+        <NavbarMenu className="bg-primary/70 text-secondary gap-y-4 py-8">
+          <NavbarMenuItem>
+            {user ? (
+              <>
+                {isImam && (
+                  <>
+                    <Link
+                      className="text-secondary text-xl font-medium w-full"
+                      href="/home"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Accueil
+                    </Link>
+                    <Link
+                      className="text-secondary text-xl font-medium w-full"
+                      href="/mosque"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Mosquées
+                    </Link>
+                    <Link
+                      className="text-secondary text-xl font-medium w-full"
+                      href="/apply"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Postuler
+                    </Link>
+                  </>
+                )}
+                {isMosqueManager && (
+                  <>
+                    <Link
+                      className="text-secondary text-xl font-medium w-full"
+                      href="/home"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Accueil
+                    </Link>
+                    <Link
+                      className="text-secondary text-xl font-medium w-full"
+                      href="/imam"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Imams
+                    </Link>
+                    <Link
+                      className="text-secondary text-xl font-medium w-full"
+                      href="/publish"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Publier
+                    </Link>
+                  </>
+                )}
+                {isAdmin && (
+                  <Link
+                    className="text-secondary text-xl font-medium w-full"
+                    href="/home"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Accueil
+                  </Link>
+                )}
+                <Link
+                  className="text-secondary text-xl font-medium w-full"
+                  href="/profile"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Mon profil
+                </Link>
+              </>
+            ) : (
+              <Link
+                className="text-secondary text-xl font-medium w-full"
+                href="/login"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Se connecter
+              </Link>
+            )}
+          </NavbarMenuItem>
+          <NavbarMenuItem>
+            <Link
+              className="text-secondary text-xl font-medium w-full"
+              href="/contact"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Contact
+            </Link>
+          </NavbarMenuItem>
+          <NavbarMenuItem>
+            <Link
+              className="text-secondary text-xl font-medium w-full"
+              href="/faq"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              FAQ
+            </Link>
+          </NavbarMenuItem>
+        </NavbarMenu>
+      </Navbar>
     </div>
   );
 };
