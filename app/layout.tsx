3152ef210d1f74b741e010d19d9e52344b2e166f
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-06-15 21:51:43 +0200

    feat: Integrate ToastProvider and update reCAPTCHA key variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import clsx from "clsx";
 import { Suspense } from "react";
 import { ReCaptchaProvider } from "next-recaptcha-v3";
+import { ToastProvider } from "@heroui/toast";
 
 import { Providers } from "./providers";
 
@@ -13,7 +14,7 @@ import Container from "@/components/container";
 import { Footer } from "@/components/footer";
 import Loading from "@/components/loading";
 import ScrollToTop from "@/components/scroll-to-top";
-import { NEXT_PUBLIC_RECAPTCHA_SITE_KEY } from "@/config/config";
+import { NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY } from "@/config/config";
 
 export default function RootLayout({
   children,
@@ -50,7 +51,10 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <ReCaptchaProvider reCaptchaKey={NEXT_PUBLIC_RECAPTCHA_SITE_KEY}>
+          <ReCaptchaProvider
+            reCaptchaKey={NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY}
+          >
+            <ToastProvider maxVisibleToasts={3} placement="bottom-center" />
             <Suspense fallback={<Loading />}>
               <Header />
               <Container>{children}</Container>
