4bb60f00604deefacbf643f6dcb21113e9e6fb80
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-06-15 21:54:02 +0200

    refactor: Simplify error handling component and update UI with navigation link

--- app/error.tsx
+++ app/error.tsx
@@ -1,27 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
+import { Link } from "@heroui/link";
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error;
-  reset: () => void;
-}) {
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }, [error]);
+import Section from "@/components/section";
 
+export default function ErrorPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <Section className="flex flex-col items-center justify-center h-screen">
       <h2 className="text-2xl font-bold text-primary">
         Une erreur est survenue !
       </h2>
-      <button className="mt-4" onClick={() => reset()}>
-        Reessayer
-      </button>
-    </div>
+      <Link className="mt-4" href="/">
+        Retour à la page d&apos;accueil
+      </Link>
+    </Section>
   );
 }
