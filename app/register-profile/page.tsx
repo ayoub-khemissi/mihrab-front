07d5f9b7636996a4f9337ba8192a79e9272c4786
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-06-27 12:16:57 +0200

    feat: Enhance RegisterProfilePage to manage user roles and render appropriate profile components

--- app/register-profile/page.tsx
+++ app/register-profile/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Loading from "@/components/loading";
+import { UserRole } from "@/types/DatabaseTypes";
+import RegisterProfileMosqueManager from "@/components/register-profile-mosque-manager";
+import RegisterProfileImam from "@/components/register-profile-imam";
+
 export default function RegisterProfilePage() {
-  return <></>;
+  const router = useRouter();
+  const [role, setRole] = useState<UserRole>(UserRole.NONE);
+
+  useEffect(() => {
+    const role = localStorage.getItem("role");
+
+    if (role !== UserRole.IMAM && role !== UserRole.MOSQUE_MANAGER) {
+      router.push("/role-selection");
+
+      return;
+    }
+    setRole(role as UserRole);
+  }, []);
+
+  if (role === UserRole.NONE) {
+    return <Loading />;
+  }
+
+  return role === UserRole.IMAM ? (
+    <RegisterProfileImam />
+  ) : (
+    <RegisterProfileMosqueManager />
+  );
 }
